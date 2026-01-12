5730cd297da372eb8b83273290e0242f8ec71c62
goldmouthkal <goldmouthkal19@gmail.com>
2026-01-12 15:26:32 +0200

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -1,7 +1,7 @@
 public class Sum {
     public static void main( String args[]){
-        int a=1;
-        int b=10;
+        int a=19;
+        int b=11;
         int c=a+b;
         System.out.println(c);
     }
